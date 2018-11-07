9460656c362760634e57784ff2d78e64f85dd783
shenxiaohan <shenxiaohan@jiehun.com.cn>
2018-11-07 18:22:42 +0800

    [ADDED] nuxt init

---
+++ nuxt.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    head: {
+        title: 'nuxt 2.x demo',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width = device-width, initial-scale = 1' },
+            { hid: 'description', name: 'desctiption', content: 'Nuxt.js 2.x project' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+    loading: {}
+}
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    <h1>Hello World</h1>
+    <img src="~/assets/images/image.gif" alt="">
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
